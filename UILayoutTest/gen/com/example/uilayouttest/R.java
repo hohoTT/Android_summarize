f0dca21c40d50954719db465f1987f8236f1da33
hohoTT <609029365@qq.com>
2016-02-23 20:42:57 +0800

    添加TableLayout布局的使用，使用表格的方式来排列控件

--- UILayoutTest/gen/com/example/uilayouttest/R.java
+++ UILayoutTest/gen/com/example/uilayouttest/R.java
@@ -1080,6 +1080,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
+        public static final int account=0x7f050049;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,7 +1092,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050049;
+        public static final int action_settings=0x7f05004c;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1122,10 +1123,12 @@ containing a value of this type.
         public static final int input_message2=0x7f050045;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int login=0x7f05004b;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int password=0x7f05004a;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1212,6 +1215,7 @@ containing a value of this type.
         public static final int relative_layout_one=0x7f03001c;
         public static final int relative_layout_two=0x7f03001d;
         public static final int support_simple_spinner_dropdown_item=0x7f03001e;
+        public static final int table_layout=0x7f03001f;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
